c96272cbe88153e5c9a628e0bd6e3faf39dff85e
Johanna Oesterreicher <johannaoest@hotmail.com>
2021-06-04 12:23:01 +0200

    WatchList

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+puts "Removing all movies..."
+Movie.destroy_all
+List.destroy_all
+
+puts "Reading movies from the API..."
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+response = RestClient.get(url)
+data = JSON.parse(response.body)
+
+puts "Creating movies..."
+data['results'].each do |info|
+  Movie.create!(
+    title: info['title'],
+    overview: info['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500#{info['poster_path']}",
+    rating: info['vote_average']
+  )
+end
+
+puts "All done 🎉"
